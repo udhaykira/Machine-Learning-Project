418cbfa7636b9381dffe804db665b1de7e434a7c
udhaykira <2003udhaykiran@gmail.com>
2024-02-11 13:56:22 +0530

    Data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from MLProject import logger
+from MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("My name is Uday")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/MLProject/utils/common.py
+++ src/MLProject/utils/common.py
@@ -13,30 +13,27 @@ from typing import Any
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """Reads a YAML file and returns its content wrapped in a ConfigBox.
+    """reads yaml file and returns
 
     Args:
-        path_to_yaml (str): Path to the YAML file.
+        path_to_yaml (str): path like input
 
     Raises:
-        ValueError: If the YAML file is empty.
-        BoxValueError: If the content of the YAML file is not in the expected format.
-        Exception: If an unexpected error occurs while reading the YAML file.
+        ValueError: if yaml file is empty
+        e: empty file
 
     Returns:
-        ConfigBox: A ConfigBox containing the content of the YAML file.
+        ConfigBox: ConfigBox type
     """
     try:
-    with open(path_to_yaml) as yaml_file:
-        content = yaml.safe_load(yaml_file)
-        if content is None:
-            raise ValueError("YAML file is empty")
-        logger.info(f"YAML file '{path_to_yaml}' loaded successfully")
-        print("YAML content:", content)  # Add this line to inspect content
-        return ConfigBox(content)       
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
     except Exception as e:
-         raise e
-
+        raise e
     
 
 
